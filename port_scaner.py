2b0b2ced25ffd3584499be47a136638f65826711
Fedor <levsjx@yandex.ru>
2025-05-08 12:56:36 +0300

    adding range of ports

--- port_scaner.py
+++ port_scaner.py
@@ -5,26 +5,60 @@ def scan(targets,ports):
         print(f"[*] Scaning port {ports[port]}")
         scan_port(targets,port)
 
+def scan_in_range(targets,ports):
+    for port in range(1,ports[0]+1):
+        print(f"[*] Scaning port {port}")
+        scan_port_in_range(targets,port)
+
+def scan_port_in_range(ipadress,port):
+    try:            #при удачном сканировании
+        sock=socket.socket()#инициализируем сокет
+        #устанавливаем соединение:
+        sock.connect((ipadress,port))
+        print(f"[+] PORT {port} IS OPENED")
+        sock.close()
+    except:
+        pass
+
+
 def scan_port(ipadress,port):
     try:            #при удачном сканировании
         sock=socket.socket()#инициализируем сокет
         #устанавливаем соединение:
         sock.connect((ipadress,port))
         print(f"[+] PORT {ports[port]} IS OPENED")
+        sock.close()
     except:
         print(f"[-] PORT {ports[port]} is closed")
 #получаем инфу от пользователя
 targets=input("[XD] Enter Targets To Scan(you can split them by \",\"): ")
 ports=[]
-how_many_ports=int(input("[XD] Enter How Many Ports You Want To Scan: "))
-for i in range (how_many_ports):
+choose=int(input("""CHOSE OPTIONS:
+      1.Scan certan ports
+      2.Scan range of ports(from "1" to input)  """))
+if choose==1:
+    how_many_ports=int(input("[XD] Enter How Many Ports You Want To Scan: "))
+    for i in range (how_many_ports):
+        i=int(input("[XD] Enter Port Number: "))
+        ports.append(i)
+    if "," in targets:
+        print("[>>] Scaning Multiple Targets")
+        for ip_adr in targets.split(","):
+            print(f"[!!] Scaning {ip_adr}")
+            scan(ip_adr,ports)
+        
+    else:
+        print(f"[!!] Scaning {targets}")
+        scan(targets,ports)
+if choose ==2:
     i=int(input("[XD] Enter Port Number: "))
     ports.append(i)
-if "," in targets:
-    print("[>>] Scaning Multiple Targets")
-    for ip_adr in targets.split(","):
-        print(f"[!!] Scaning {ip_adr}")
-        scan(ip_adr,ports)
-else:
-    print(f"[!!] Scaning {targets}")
-    scan(targets,ports)
+    if "," in targets:
+        print("[>>] Scaning Multiple Targets")
+        for ip_adr in targets.split(","):
+            print(f"[!!] Scaning {ip_adr}")
+            scan_in_range(ip_adr,ports)
+        
+    else:
+        print(f"[!!] Scaning {targets}")
+        scan_in_range(targets,ports)
